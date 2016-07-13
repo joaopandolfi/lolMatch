d308c2bcc582bb0cc36f845e2938de222869dbcc
João Carlos <joaopandolfi@gmail.com>
2016-07-13 20:58:43 -0300

    Bd

--- BD/insertspostgre.sql
+++ BD/insertspostgre.sql
@@ -4,7 +4,8 @@
 INSERT INTO titulo (idtitulo,nome,valor)
 VALUES	(1 ,'Safadao' ,9000 ),
  		(2 ,'Matador de noob' ,5000 ),
-	   	(3 ,'Pela égua' ,4000 );
+ 		(3 ,'Passa nois Paulim' ,5000 ),
+	   	(4 ,'Pela égua' ,4000 );
 
 /*
 * Insercao itens
@@ -15,6 +16,12 @@ VALUES	(1 ,'Convites' ,2 ,200 ),
 	   	(3 ,'Convites promoção 4x' ,8 ,200 );
 
 
+/*
+* Insercao de usuario
+*/
+
+SELECT combo_insert('brtt','Teste123','t@1.com','123');
+SELECT combo_insert('seu madruga','RobinDoTeste','seu@madruga.com','123');
 
 /*
 * Insercao lolaccount
@@ -63,25 +70,25 @@ VALUES	(1 ,1,1 ),
 
 /*
 * Insercao chat
-*/
+
 INSERT INTO chat (idmsg,message,read,usuario_idusuario_sender,usuario_idusuario_receiver)
 VALUES	(1 ,'bacon é mt bom' ,1 ,1 ,2 ),
  		(2 ,'quer jogar com eu tio?' ,0 ,1 ,2 ),
 	   	(3 ,'sou bronze, me sobe de elo?' ,0 ,2 ,1 );
-
+*/
 /*
 * Insercao contratos
-*/
+
 INSERT INTO contratos (idcontratos,valor,data,usuario_idusuario_contratante,usuario_idusuario_contratado)
 VALUES	(1 ,2 ,'2016-7-04' ,1 ,2 ),
  		(2 ,100 ,'2016-7-04' ,3 ,2 ),
 	   	(3 ,30 ,'2016-7-04' ,3 ,2 );
-
+*/
 /*
 * Insercao matchs
-*/
+
 INSERT INTO matchs (idmatchs,data,verifyed,usuario_idusuario_convidado,usuario_idusuario_convidante)
 VALUES	(1 ,'2016-7-04' ,0 ,1 ,2 ),
  		(2 ,'2016-7-04' ,0 ,1 ,3 ),
 	   	(3 ,'2016-7-04' ,0 ,1 ,3 );
-
+*/
